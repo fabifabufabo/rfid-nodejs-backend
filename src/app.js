e32f303a829dc7b8c5f3edeb4102c5c78e0d3425
Fábio Fernandes <dev.fabiofernandes@gmail.com>
2025-04-21 04:06:23 -0300

    feat: add CORS support and clean up user data in JSON files

--- src/app.js
+++ src/app.js
@@ -2,15 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('./middleware/logger');
 const rfidRoutes = require('./routes/rfid');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use(logger);
 app.use('/rfid', rfidRoutes);
 
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-}); 
+});
